9c7acede1d190e0c545349671cec8f16aa3a70bc
krzyskopytek <krzys.kopytek@hotmail.com>
2016-05-17 10:35:36 -0400

    init

---
+++ everyDo/everyDo/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  everyDo
+//
+//  Created by Krzysztof Kopytek on 2016-05-17.
+//  Copyright © 2016 Krzysztof Kopytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ everyDo/everyDo/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  everyDo
+//
+//  Created by Krzysztof Kopytek on 2016-05-17.
+//  Copyright © 2016 Krzysztof Kopytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
